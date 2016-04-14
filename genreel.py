9693fd4ff888f204bf3bbc6709db46a39af41007
Peter Neubauer <peterneubauer2@gmail.com>
2016-04-14 19:49:52 +0200

    New option --method for genreel, can generate a variety of good test cases

--- genreel.py
+++ genreel.py
@@ -23,15 +23,18 @@ def make_reel(N,S):
 	R = sample_pool(pool,N)
 	return ''.join(R)
 
-# Old implementation: simple random substrings
-# def observations(reel,N,S,T):
-# 	for i in range(0,N):
-# 		L = random.randint(S,T)
-# 		k = random.randint(0,len(reel)-L)
-# 		yield reel[k:k+L]
+# Simple method: take N random rubstrings from reel of length 3-8 symbols
+def obs_random(reel,N):
+	R = len(reel)
+	reeloop = reel*2
+	for i in range(0,N):
+		L = random.randint(3,8)
+		k = random.randint(0,R)
+		yield reeloop[k:k+L]
 
-# Updated implementation: cut the reel into segments at random cut points, each of which becomes a piece, with a predefined overlap of 2
-def observations(reel,N):
+# Generates observations with small overlap.
+# Method: cut the reel into segments at random cut points, each of which becomes a piece, with a predefined overlap of 2
+def obs_min_overlap(reel,N):
 	R = len(reel)
 	cuts = random.sample(range(0,R), N)
 	cuts.sort()
@@ -45,21 +48,30 @@ def observations(reel,N):
 
 		yield (reel*2)[low:high]
 
+# Generates len(reel) observations with largest possible overlap.
+# Method: for every observation, one symbol is left out of the reel.
+# The number of observations (N) is ignored.
+def obs_max_overlap(reel,N):
+	for i in range(0,len(reel)):
+		yield reel[i+1:] + reel[:i]
+
 def handle_args():
 	parser = argparse.ArgumentParser(description='Random generator for reels.py problems')
-	parser.add_argument('reel_size', type=int, help='number of symbols in the generated reel')
 	parser.add_argument('sym_count', type=int, help='number of distinct symbols in the alphabet')
+	parser.add_argument('reel_size', type=int, help='number of symbols in the generated reel')
 	parser.add_argument('obs_count', type=int, help='number of observations to generate')
+	parser.add_argument('-m', '--method', type=str, choices=['random','min_overlap','max_overlap'], default='random', help='how to generate the list of observations')
 	# NOTE: observation size is now determined by the random size of the piece
 	# parser.add_argument('obs_min', type=int, help='minimum size of an observation')
 	# parser.add_argument('obs_max', type=int, help='maximum size of an observation')
 	args = parser.parse_args()
-	return args.reel_size, args.sym_count, args.obs_count # , args.obs_min, args.obs_max
+	return args.sym_count, args.reel_size, args.obs_count, args.method # , args.obs_min, args.obs_max
 
-def random_observations(reel_size, sym_count, obs_count): # , obs_min, obs_max):
+def random_observations(sym_count, reel_size, obs_count, method): # , obs_min, obs_max):
 	random.seed()
 	reel = make_reel(reel_size, sym_count)
-	obs = list(observations(reel, obs_count)) #, obs_min, obs_max))
+	obs_func = globals()['obs_' + method]
+	obs = list(obs_func(reel, obs_count)) #, obs_min, obs_max))
 	random.shuffle(obs)      # input order should not matter, but shuffle just to be safe
 	return (reel, obs)
 
